16b145a3ea8b76567840f398357dc284b1d6ed9a
13263955567 <1498423299@qq.com>
2020-10-14 14:50:00 +0800

    Update tgchannel.go

--- pkg/getter/tgchannel.go
+++ pkg/getter/tgchannel.go
@@ -6,8 +6,8 @@ import (
 	"sync"
 
 	"github.com/gocolly/colly"
-	"github.com/Sansui233/proxypool/pkg/proxy"
-	"github.com/Sansui233/proxypool/pkg/tool"
+	"github.com/13263955567/proxypool/pkg/proxy"
+	"github.com/13263955567/proxypool/pkg/tool"
 )
 
 func init() {
